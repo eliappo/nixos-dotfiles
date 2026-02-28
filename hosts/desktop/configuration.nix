91bf515b2077ec843ff4b97d556657f920201205
eliappo <237402327+eliappo@users.noreply.github.com>
2026-02-28 11:15:29 +0100

    Add: Adding ssh key to agent for desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,6 +5,12 @@
 
   networking.hostName = "nixos-desktop";
 
+  users.users.elias = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrmlWbyrXyqEI8nP/N31d1yfT314rk3Jr7DS47f6Q27 desktop ssh"
+    ];
+  };
+
   # Desktop-specific GPU config (adjust to your actual GPU)
   services.xserver.videoDrivers = [ "nvidia" ]; # or "amdgpu"
   hardware.nvidia.open = true; # uncomment if nvidia
